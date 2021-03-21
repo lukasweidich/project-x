8bb4076814aa26a412aafb1807a5b25b5a8bd9e4
Lukas <lukas.weidich@gmx.de>
2021-03-21 15:38:26 +0100

    🐛 add types to links

--- components/link/ExternalLink.tsx
+++ components/link/ExternalLink.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 
-const ExternalLink = ({ href = "", children, ...rest }) => {
+const ExternalLink = ({
+	href = "",
+	children,
+	...rest
+}: {
+	href: string;
+	children: string | React.ReactNode;
+	[key: string]: any;
+}) => {
 	return (
 		<a href={href} {...rest} target="_blank" rel="noopener noreferrer">
 			{children}
--- components/link/InternalLink.tsx
+++ components/link/InternalLink.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const InternalLink = ({ href = "", children, ...rest }) => {
+const InternalLink = ({
+	href = "",
+	children,
+	...rest
+}: {
+	href: string;
+	children: string | React.ReactNode;
+	[key: string]: any;
+}) => {
 	const router = useRouter();
 
 	const className = router.pathname === href ? "active" : "";
