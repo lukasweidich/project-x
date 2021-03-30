5ee66556b0a40feb22e46b6b32084b8405d873cf
Lukas <lukas.weidich@gmx.de>
2021-03-30 10:16:22 +0200

    💄 add loading animation for group previe

--- components/group/GroupPreview.tsx
+++ components/group/GroupPreview.tsx
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Avatar, AvatarGroup } from "@chakra-ui/avatar";
 import { SPACING_IN_PX } from "../../utils/constants";
 import { Box } from "@chakra-ui/react";
 import { GroupInterface } from "./../../db/types/Group";
 import InternalLink from "./../link/InternalLink";
 import { Heading } from "@chakra-ui/layout";
+import { Tooltip, Skeleton, SimpleGrid, Container } from "@chakra-ui/react";
+import { getUser } from "../../utils/api";
+import useAuth from "../../hooks/useAuth";
+import { UserInterface } from "./../../db/types/User";
 
 const GroupPreview = ({ group }: { group: GroupInterface }) => {
+	const [loading, setLoading] = useState<boolean>(true);
+	const [members, setMembers] = useState<Array<UserInterface>>([]);
+	const { token } = useAuth();
+
+	useEffect(() => {
+		const init = async () => {
+			const members = await Promise.all(
+				group.members.map(async (memberId) => await getUser(memberId, token)),
+			);
+			setMembers(members);
+			setLoading(false);
+		};
+		init();
+	}, [token]);
+
 	return (
-		<InternalLink href="#">
-			<Box borderRadius="lg" borderWidth="1px" p={SPACING_IN_PX}>
-				<Heading isTruncated size="md">
-					{group.name}
-				</Heading>
-				<AvatarGroup max={2}>
-					{group.members.map((member) => {
-						//TODO: add fetching of members
-						return <Avatar />;
-					})}
-				</AvatarGroup>
-			</Box>
+		<InternalLink href={`#${group._id}`}>
+			<Container>
+				<Box borderRadius="lg" borderWidth="1px" p={SPACING_IN_PX}>
+					<SimpleGrid columns={1} spacing={SPACING_IN_PX}>
+						<Heading isTruncated size="md">
+							{group.name}
+						</Heading>
+						{loading ? (
+							<Skeleton isLoaded={!loading} height="12" />
+						) : (
+							<AvatarGroup max={3}>
+								{members.map((member) => {
+									const fullName = `${member.firstName} ${member.lastName}`;
+									return (
+										<Tooltip label={fullName}>
+											<Avatar src={member.avatar} name={fullName} />
+										</Tooltip>
+									);
+								})}
+							</AvatarGroup>
+						)}
+					</SimpleGrid>
+				</Box>
+			</Container>
 		</InternalLink>
 	);
 };
--- components/misc/CheckAuthAndUpdateUser.tsx
+++ components/misc/CheckAuthAndUpdateUser.tsx
@@ -4,21 +4,29 @@ import { logOut } from "../../actions/authActions";
 import { updateUser } from "../../actions/userActions";
 import useAuth from "../../hooks/useAuth";
 import { getUser } from "../../utils/api";
-
+import { PathNames } from "../../utils/constants";
+import { useRouter } from "next/router";
+const { ROOT } = PathNames;
 const CheckAuthAndUpdateUser = () => {
 	/**
-	 * once the application is loaded, update user data and make sure token is still valid
+	 * once the application is loaded, update user data and make sure token and password are still valid
 	 * else, perform log out
 	 */
 	const dispatch = useDispatch();
 	const { token, user } = useAuth();
+	const router = useRouter();
 	useEffect(() => {
 		const checkAuthAndUpdateUser = async () => {
 			try {
+				/**
+				 * check for token and password validity
+				 */
+				// TODO: check if password is valid
 				const userFromDb = await getUser(user._id, token);
 				dispatch(updateUser(userFromDb));
 			} catch (error) {
-				dispatch(logOut());
+				await dispatch(logOut());
+				await router.push(ROOT);
 			}
 		};
 		checkAuthAndUpdateUser();
